4c9a491ba99757969ba10df6711f0e364570033b
Canice Musa Quraishy <canice.quraishy@strathmore.edu>
2025-06-22 12:47:43 +0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   const overlay = document.getElementById("inviteOverlay");
+
+  // Delay the show and background transition
   setTimeout(() => {
     overlay.classList.add("show");
-  }, 2000);
+  }, 3000); // 3 seconds
 
   const form = document.getElementById("rsvpForm");
   const thankYou = document.getElementById("thankYou");
 
-  form.addEventListener("submit", function(e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
     thankYou.style.display = "block";
     form.reset();
